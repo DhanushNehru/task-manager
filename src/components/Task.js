176fde3d7b80ed77ce95275e7ad4b4b35f4defa4
doriroz <doriroz@gmail.com>
2024-07-13 09:55:35 +0300

    task description

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,6 +6,8 @@ import removeIcn from "../Images/remove.png";
 
 const Task = ({ task }) => {
   const [isDisabled, setIsDisabled] = useState(true);
+  const [isExpend, setIsExpend] = useState(false);
+
   const dispatch = useDispatch();
 
   const handleRemoveTask = () => {
@@ -25,33 +27,64 @@ const Task = ({ task }) => {
     e.keyCode === 13 && setIsDisabled(true);
   };
 
+  const styles = {
+    tskname: {
+      display: "flex",
+      gap: "1rem",
+      marginTop: "1rem",
+    },
+    editImg: {
+      width: "1.2rem",
+    },
+    editBtn: {
+      border: "none",
+      borderRadius: "10px",
+    },
+    input: {
+      border: "none",
+      backgroundColor: "white",
+      fontSize: "25px",
+    },
+    description: {
+      backgroundColor: "#eee",
+      padding: "1rem",
+      borderRadius: "10px",
+      fontFamily: "Ariel",
+      fontSize: "25px",
+      color: "#1c0738",
+      display: isExpend ? "block" : "none",
+    },
+    isexpend: {
+      width: "5rem",
+      fontSize: "30px",
+      cursor: "pointer",
+    },
+  };
+
   return (
-    <div style={{ display: "flex", gap: "1rem", marginTop: "1rem" }}>
-      <input
-        type="text"
-        style={{ border: "none", backgroundColor: "white" }}
-        disabled={isDisabled}
-        defaultValue={task.name}
-        onKeyDown={handleEditTask}
-      />
-      {/* <span>{task.name}</span> */}
-      <button onClick={handleOpenInput} style={styles.editBtn}>
-        <img src={editIcn} style={styles.editImg} />
-      </button>
-      <button onClick={handleRemoveTask} style={styles.editBtn}>
-        <img src={removeIcn} style={styles.editImg} />
-      </button>
-    </div>
+    <>
+      <div style={styles.tskname}>
+        <span style={styles.isexpend} onClick={() => setIsExpend(!isExpend)}>
+          {isExpend ? <span>&#8681;</span> : <span>&#8680;</span>}
+        </span>
+        <input
+          type="text"
+          style={styles.input}
+          disabled={isDisabled}
+          defaultValue={task.name}
+          onKeyDown={handleEditTask}
+        />
+
+        <button onClick={handleOpenInput} style={styles.editBtn}>
+          <img src={editIcn} style={styles.editImg} />
+        </button>
+        <button onClick={handleRemoveTask} style={styles.editBtn}>
+          <img src={removeIcn} style={styles.editImg} />
+        </button>
+      </div>
+      <p style={styles.description}>{task.description}</p>
+    </>
   );
 };
 
-const styles = {
-  editImg: {
-    width: "1.2rem",
-  },
-  editBtn: {
-    border: "none",
-    borderRadius: "10px",
-  },
-};
 export default Task;
