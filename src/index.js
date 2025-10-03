f8055ff463ebbf5f6ea8a6df5a4ee75815af83ca
Saad Arqam <saadarqam@Saads-MacBook-Pro.local>
2025-10-03 22:21:11 +0530

    chore: downgrade to React 18 and fix compatibility with MUI 5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
